237e1c3d534f6452bd623bd8ba721838f95a22c6
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2022-01-28 08:54:18 +0100

    heroku

--- config/config.js
+++ config/config.js
@@ -1,25 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || '',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || '',
-    host: process.env.DB_HOST || '',
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.DB_USER || '',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || '',
-    host: process.env.DB_HOST || '',
-    dialect: 'postgres'
+    username: process.env.User || "",
+    password: process.env.Password || "",
+    database: process.env.Name || "",
+    host: process.env.Host || "",
+    dialect: "postgres",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    use_env_variable: "DATABASE_URL",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
